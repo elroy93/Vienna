45d7d006f05babb31417f0cf135c076550dcbdfe
elroysu <elroysu@tencent.com>
2021-12-28 13:59:12 +0800

    工具类

--- Vienna-processor/src/main/java/com/onemuggle/vienna/common/ReNamedTreeTranslator.java
+++ Vienna-processor/src/main/java/com/onemuggle/vienna/common/ReNamedTreeTranslator.java
@@ -12,6 +12,25 @@ import java.util.Map;
 import java.util.Stack;
 import java.util.stream.Collectors;
 
+/**
+ * 在代码块中的字段中标注  @ReNamed(value = "R_11",isInterface = false)
+ * 可以生成相应的内部类。
+ * 相当于把
+ *
+ * ' @ReNamed(value = "R_11",isInterface = true)
+ *   Runnable r = new Runnable() {
+ *     xxxx
+ *   } '
+ *
+ * 改成
+ *
+ *  ' class R_11 implements Runnable() {
+ *       xxxx
+ *   }
+ *   Runnable r = new R_11(); '
+ *
+ *
+ */
 public class ReNamedTreeTranslator extends TreeTranslator {
 
 
@@ -116,52 +135,6 @@ public class ReNamedTreeTranslator extends TreeTranslator {
         jcBlock.stats = newBlockStatements.toList();
     }
 
-
-//    @Override
-//    public void visitMethodDef(JCTree.JCMethodDecl jcMethodDecl) {
-//        super.visitMethodDef(jcMethodDecl);
-//        String s = jcMethodDecl.toString();
-//
-//
-//        if (!jcMethodDecl.getName().toString().equals("rStatic")) {
-//            return;
-//        }
-//
-//        JCTree.JCBlock bodyBlock = jcMethodDecl.body;
-//
-//        ListBuffer<JCTree.JCStatement> newLines = new ListBuffer<>();
-//        newLines.add(bodyBlock.stats.get(0));
-//        newLines.add(bodyBlock.stats.get(1));
-//
-//
-//        JCTree.JCNewClass newClassDecl = null;
-//        for (JCTree.JCStatement stat : bodyBlock.stats) {
-//            if (stat instanceof JCTree.JCVariableDecl) {
-//                JCTree.JCVariableDecl variableDeclaration = (JCTree.JCVariableDecl) stat;
-//                JCTree.JCModifiers modifiers = variableDeclaration.getModifiers();
-//                if (CollectionUtil.isNotEmpty(modifiers.getAnnotations()) && modifiers.toString().contains("@ReNamed(")) {
-//                    String newClassName = (String) ((JCTree.JCLiteral) modifiers.getAnnotations().get(0).getArguments().get(0).getTree()).getValue();
-//
-//                    JCTree.JCClassDecl def = ((JCTree.JCNewClass) variableDeclaration.getInitializer()).def;
-//                    newClassDecl = (JCTree.JCNewClass) variableDeclaration.getInitializer();
-//                    def.name = Utils.names.fromString(newClassName);
-//                    def.implementing = List.of(Utils.treeMaker.Ident(Utils.names.fromString("Runnable")));
-//
-//                    newLines.add(def);
-//                }
-//            }
-//
-////            ((JCTree.JCClassDecl)((JCTree.JCNewClass)((JCTree.JCVariableDecl)bodyBlock.stats.get(1)).init).def).implementing = List.of(VennaUtils.treeMaker.Ident(VennaUtils.names.fromString("java.lang.Runnable")));
-//            System.out.println(" == newClassDecl \n" + newClassDecl);
-//        }
-//
-//        newLines.add(bodyBlock.stats.get(3));
-//        newLines.add(bodyBlock.stats.get(4));
-//        newLines.add(bodyBlock.stats.get(5));
-//
-//        bodyBlock.stats = newLines.toList();
-//
-//    }
 
     static class InnerClazzMeta {
 
